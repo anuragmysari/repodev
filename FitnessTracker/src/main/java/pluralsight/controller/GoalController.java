2338d439db615c0d3748ea60ad90feefdf7ceb3b
Anurag Mysari <anuragmysari@gmail.com>
2018-07-29 18:41:30 -0400

    addGoal

---
+++ FitnessTracker/src/main/java/pluralsight/controller/GoalController.java
@@ -0,0 +1,30 @@
+package pluralsight.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import pluralsight.model.Goal;
+
+@Controller
+@SessionAttributes("goal")
+public class GoalController {
+
+	@RequestMapping(value = "addGoal", method = RequestMethod.GET)
+	public String addGoal(Model model) {
+		Goal goal = new Goal();
+		model.addAttribute("goal", goal);
+		return "addGoal";
+	}
+
+	@RequestMapping(value = "addGoal", method = RequestMethod.POST)
+	public String updateGoal(@ModelAttribute("goal") Goal goal) {
+
+		System.out.println(goal.getMinutes());
+
+		return "redirect:addMinutes.html";
+	}
+}
